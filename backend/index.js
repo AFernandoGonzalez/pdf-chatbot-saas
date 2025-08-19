4e7a69ecbf0c938d62cc0488ad44a428f1cff7b7
Alvaro Gonzalez <alvarogonzalez@Alvaros-Air.lan>
2025-08-19 19:29:18 -0400

    updated port

--- backend/index.js
+++ backend/index.js
@@ -31,8 +31,9 @@ app.use((req, res) => {
 });
 
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
+
 
 export default app;
