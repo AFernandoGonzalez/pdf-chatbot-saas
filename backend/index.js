e2bf9d84d070ce5b185cb1c05baa6e7e65669c43
Alvaro Gonzalez <alvarogonzalez@Alvaros-Air.lan>
2025-08-19 20:09:28 -0400

    updated port

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import connectDB from './config/db.js';
 dotenv.config();
 connectDB();
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
