f188c46041e8297717a169a17b27c739ee01482b
Alvaro Gonzalez <alvarogonzalez@Alvaros-Air.lan>
2025-08-20 19:55:44 -0400

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -9,13 +9,14 @@ import fileRoutes from './routes/fileRoutes.js';
 import connectDB from './config/db.js';
 
 dotenv.config();
+
 connectDB();
+
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
-
 
 app.get('/api', (req, res) => {
   res.send('Welcome to the PDF Chatbot API');
@@ -26,14 +27,8 @@ app.use('/api/upload', uploadRoutes);
 app.use('/api/chat', chatRoutes);
 app.use('/api/files', fileRoutes);
 
-app.use((req, res) => {
-  res.status(404).json({ error: "Route not found" });
-});
-
-
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
-
 
 export default app;
